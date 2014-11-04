cceb4f22148122e84c3106bf9b537fb9fb571c32
spaztic1215 <spaztick@gmail.com>
2014-11-04 15:35:25 -0800

    ALS recommender in Spark Python

---
+++ recommender/recommendations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from pyspark.mllib.recommendation import ALS
+from numpy import array
+
+# Load and parse the Rating data
+data = sc.textFile("~/hadoop-fundamentals/data/dating/ratings.csv")
+
+ratings = data.map(lambda line: array([float(x) for x in line.split(',')]))
+
+# Build the recommendation model using ALS
+rank = 100
+numIterations = 10
+model = ALS.train(ratings, rank, numIterations)
+
+# Evaluate the model on training data
+testdata = ratings.map(lambda p: (int(p[0]), int(p[1])))
+predictions = model.predictAll(testdata).map(lambda r: ((r[0], r[1]), r[2]))
+
+ratesAndPreds = ratings.map(lambda r: ((r[0], r[1]), r[2])).join(predictions)
+
+MSE = ratesAndPreds.map(lambda r: (r[1][0] - r[1][1])**2).reduce(lambda x, y: x + y)/ratesAndPreds.count()
+
+print("Mean Squared Error = " + str(MSE))
