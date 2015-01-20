735c279684eb944e96d8febbfb8682900dc42e92
spaztic1215 <spaztick@gmail.com>
2015-01-19 18:28:16 -0800

    Fix path error for load data command

--- hive/log_data/load_apache_log_data.sql
+++ hive/log_data/load_apache_log_data.sql
@@ -1,2 +1,2 @@
 USE log_data;
-LOAD DATA INPATH '/statistics/log_data/apache.log' OVERWRITE INTO TABLE apache_log;
+LOAD DATA INPATH 'statistics/log_data/apache.log' OVERWRITE INTO TABLE apache_log;
