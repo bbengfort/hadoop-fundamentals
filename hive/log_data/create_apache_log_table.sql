9b192a590a7247bc0aeb8b1e5985b570fb974ff9
spaztic1215 <spaztick@gmail.com>
2015-01-19 18:25:19 -0800

    Add log_data HQL

---
+++ hive/log_data/create_apache_log_table.sql
@@ -0,0 +1,18 @@
+CREATE TABLE apache_log (
+   host STRING,
+   identity STRING,
+   user STRING,
+   time STRING,
+   request STRING,
+   status STRING,
+   size STRING,
+   referer STRING,
+   agent STRING
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.contrib.serde2.RegexSerDe'
+  WITH SERDEPROPERTIES (
+     "input.regex" = "([^ ]*) ([^ ]*) ([^ ]*) (-|\\[[^\\]]*\\]) ([^ \"]*|\"[^\"]*\") (-|[0-9]*) (-|[0-9]*)(?: ([^ \"]*|\".*\") ([^ \"]*|\".*\"))?",
+     "output.format.string" = "%1$s %2$s %3$s %4$s %5$s %6$s %7$s %8$s %9$s"
+  )
+STORED AS TEXTFILE;
+
---
+++ hive/log_data/load_apache_log_data.sql
@@ -0,0 +1,2 @@
+USE log_data;
+LOAD DATA INPATH '/statistics/log_data/apache.log' OVERWRITE INTO TABLE apache_log;
